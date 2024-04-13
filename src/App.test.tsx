3e3a410c846f5556a234bcc578c9fc3aca07da63
Dylan Wulf <dmw@storis.com>
2024-04-12 21:06:39 -0400

    reproduce weird timer issue

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,53 @@
-import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { describe, expect, test, beforeEach, afterEach, vi } from 'vitest';
+import { render, screen, fireEvent, act } from '@testing-library/react';
 import App from './App';
 
+beforeEach(() => {
+  vi.useFakeTimers({
+    now: new Date('2021-02-02T16:50:26.379Z'),
+    toFake: [
+      // everything except for 'performance', which causes errors in browsers
+      'Date',
+      'cancelAnimationFrame',
+      'cancelIdleCallback',
+      'clearImmediate',
+      'clearInterval',
+      'clearTimeout',
+      'hrtime',
+      // 'performance',
+      'nextTick',
+      'queueMicrotask',
+      'requestAnimationFrame',
+      'requestIdleCallback',
+      'setImmediate',
+      'setInterval',
+      'setTimeout',
+    ],
+  });
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
+
 describe('Test Suite', () => {
-  test('Test', () => {
+  test('Test', async () => {
     render(<App />);
-    expect(
-      screen.getByText('Click on the Vite and React logos to learn more')
-    ).toBeInTheDocument();
+    const button = screen.getByRole('button', { name: 'Begin Payment' });
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+    expect(screen.getByText('Time Remaining: 02:00')).toBeInTheDocument();
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(1000);
+    });
+
+    expect(screen.getByText('Time Remaining: 01:59')).toBeInTheDocument();
+
+    await act(async () => {
+      await vi.advanceTimersByTimeAsync(4000);
+    });
+
+    expect(screen.getByText('Payment Successful')).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,83 @@
-import { useRef, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
+const doPayment = () =>
+  new Promise<string>((resolve) => {
+    window.setTimeout(resolve, 5000, 'success');
+  });
+
+const getTimeRemainingMessage = (timeRemainingMs: number) => {
+  const secondsRemaining = Math.ceil(timeRemainingMs / 1000);
+  const minutesToDisplay = Math.floor(secondsRemaining / 60)
+    .toString()
+    .padStart(2, '0');
+  const secondsToDisplay = (secondsRemaining % 60).toString().padStart(2, '0');
+  return `Time Remaining: ${minutesToDisplay}:${secondsToDisplay}`;
+};
+
+const useTimer = (updateStatus: (newStatus: string) => void) => {
+  // refs are necessary to ensure that setInterval is calling the latest callbacks
+  const updateStatusRef = useRef(updateStatus);
+  useEffect(() => {
+    updateStatusRef.current = updateStatus;
+  });
+
+  const [endTimestamp, setEndTimestamp] = useState<number | null>(null); // null if timer is not active
+
+  const startTimer = (timeLimitMs: number) => {
+    setEndTimestamp(Date.now() + timeLimitMs);
+    updateStatusRef.current(getTimeRemainingMessage(timeLimitMs));
+  };
+
+  const stopTimer = () => {
+    setEndTimestamp(null);
+  };
+
+  useEffect(() => {
+    if (endTimestamp !== null) {
+      const intervalId = window.setInterval(() => {
+        const milliSecondsRemaining = endTimestamp - Date.now();
+        if (milliSecondsRemaining <= 0) {
+          stopTimer();
+          updateStatusRef.current('Timed Out');
+        } else {
+          updateStatusRef.current(
+            getTimeRemainingMessage(milliSecondsRemaining)
+          );
+        }
+      }, 1000);
+
+      return () => {
+        window.clearInterval(intervalId);
+      };
+    }
+    return undefined;
+  }, [endTimestamp]);
+
+  return { startTimer, stopTimer };
+};
+
 function App() {
-  const [count, setCount] = useState(0);
-  const ref = useRef();
+  const [status, setStatus] = useState('Ready');
+
+  const { startTimer, stopTimer } = useTimer(setStatus);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <div>{status}</div>
+        <button
+          onClick={() => {
+            startTimer(120000);
+            doPayment().then(() => {
+              stopTimer();
+              setStatus('Payment Successful');
+            });
+          }}
+        >
+          Begin Payment
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
